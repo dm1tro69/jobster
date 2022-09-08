e5227813e527d57a0ad1b4097821c9dbba2d3057
Dmytro Voronov <dimolg22@gmail.com>
2022-09-08 18:30:37 +0300

    add api for login and register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,6 +3,9 @@ import Wrapper from "../assets/wrappers/RegisterPage";
 import {Logo} from "../components";
 import FormRow from "../components/FormRow";
 import {toast} from "react-toastify";
+import {useDispatch, useSelector} from "react-redux";
+import {loginUser, registerUser} from "../features/user/userSlice";
+import {useNavigate} from "react-router-dom";
 
 
 const initialState = {
@@ -13,8 +16,11 @@ const initialState = {
 }
 
 const Register = () => {
-
+     const navigate = useNavigate()
     const [values, setValues] = useState(initialState)
+    const {user, isLoading} = useSelector(state => state.user)
+    const dispatch = useDispatch()
+
 
     const handleChange = (e) => {
         const name = e.target.name
@@ -26,13 +32,22 @@ const Register = () => {
         const {name, email, password, isMember} = values
         if (!email || !password || (!isMember && !name)){
             toast.error('Please Fill Out All Fields')
-
+            return;
         }
+        if (isMember){
+            dispatch(loginUser({email: email, password: password}))
+            return;
+        }
+        dispatch(registerUser({name, email, password}))
     }
 
     useEffect(()=> {
-
-    }, [])
+       if (user){
+           setTimeout(()=> {
+               navigate('/')
+           }, 3000)
+       }
+    }, [user, navigate])
 
     const toggleMember = () => {
       setValues({...values,isMember: !values.isMember })
@@ -74,6 +89,7 @@ const Register = () => {
                 </button>
                 <p>{values.isMember ? 'Not a member yet?': 'Already a member?'}
                     <button
+                        disabled={isLoading}
                         className={'member-btn'}
                         type={'button'}
                         onClick={toggleMember}
